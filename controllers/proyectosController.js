df1e6e58ae4262b44b9299f3c4114cfa904add68
Alvagit25 <alvarodesegovia@gmail.com>
2021-12-03 19:07:02 +0100

    insertar registro

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,3 +1,5 @@
+const Proyectos = require("../models/Proyectos");
+
 exports.proyectosHome = (req, res) => {
   //res.send("Hola Index");
   res.render("index", {
@@ -13,30 +15,33 @@ exports.formularioProyecto = (req, res) => {
 };
 
 exports.nuevoProyecto = (req, res) => {
-    // Enviar a la consola lo que el usuario escriba
-    console.log(req.body)
+  // Enviar a la consola lo que el usuario escriba
+  console.log(req.body);
 
-    // validar que tengamos algo en el input
-    const {nombre} = req.body
+  // validar que tengamos algo en el input
+  const { nombre } = req.body;
 
-    let errores = []
+  let errores = [];
 
-    if (!nombre) {
-        errores.push({'texto': 'Agrega un Nombre al Proyecto'})
-    }
+  if (!nombre) {
+    errores.push({ texto: "Agrega un Nombre al Proyecto" });
+  }
 
-    // si hay errores
-    if(errores.length > 0){
-        res.render('nuevoProyecto', {
-            nombrePagina: 'Nuevo Proyecto',
-            errores
-        })
-    }else{
-        // No hay errores
-        // Insertar en la BD
-    }
+  // si hay errores
+  if (errores.length > 0) {
+    res.render("nuevoProyecto", {
+      nombrePagina: "Nuevo Proyecto",
+      errores,
+    });
+  } else {
+    // No hay errores
+    // Insertar en la
+    Proyectos.create({ nombre })
+      .then(() => console.log("Insertado correctamente"))
+      .catch(error => console.log(error));
+  }
 
-    //res.send("enviaste el Formulario");
+  //res.send("enviaste el Formulario");
 };
 
 /* exports.nosotros = (req, res) => {
