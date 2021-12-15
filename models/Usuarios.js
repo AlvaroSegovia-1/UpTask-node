3fd6b9c597b4c39f36e35ff77bd8ee40e0d3b0d0
Alvagit25 <alvarodesegovia@gmail.com>
2021-12-15 10:55:48 +0100

    hashear password

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,22 +1,35 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
 const Proyectos = require("../models/Proyectos");
+const bcrypt = require("bcrypt-nodejs");
 
-const Usuarios = db.define("usuarios", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Usuarios = db.define(
+  "usuarios",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: Sequelize.STRING(60),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(60),
+      allowNull: false,
+    },
   },
-  email: {
-    type: Sequelize.STRING(60),
-    allowNull: false,
+  {
+    hooks: {
+      beforeCreate(usuario) {
+        //console.log("creando nuevo usuario");
+        //console.log(usuario);
+        usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10));
+      },
+    },
   },
-  password: {
-    type: Sequelize.STRING(60),
-    allowNull: false,
-  },
-});
+);
 Usuarios.hasMany(Proyectos);
 
 module.exports = Usuarios;
